158fb4fc962436669a9c4f470016fe74540ad18d
Kender <kendermendoza591@outlook.com>
2020-05-14 19:28:21 -0600

    Se organizo la carpeta del projecto

---
+++ src/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>Hola mundo desde node js</h1>");
+  })
+  .listen(3000, (err) => {
+    if (err) throw err;
+
+    console.log("Server on port 3000");
+  });
