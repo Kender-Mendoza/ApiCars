a855423acf06e26c2bb64f9fdf96834e3f1ab952
Kender <kendermendoza591@outlook.com>
2020-05-14 20:19:53 -0600

    Db Connection

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 "use strict";
 
 const http = require("http");
+const db = require('./config/dbConnection')
 
 http
-  .createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Hola mundo desde node js</h1>");
-  })
-  .listen(3000, (err) => {
-    if (err) throw err;
+    .createServer((req, res) => {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Hola mundo desde node js</h1>");
+    })
+    .listen(3000, (err) => {
+        if (err) throw err;
 
-    console.log("Server on port 3000");
-  });
+        console.log("Server on port 3000");
+    });
