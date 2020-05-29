e10e1741f6b78f44f7b16864742bbe932907e735
Kender <kendermendoza591@outlook.com>
2020-05-28 21:53:19 -0600

    Se creo CarsController

---
+++ src/controller/cars.controller.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const carModel = require('../model/cars.model')
+
+/*
+    Funcion para convertir el resultado de la consulta a un json 
+    y poder enviarlo.
+*/
+function createJson(array){
+    let data = {}
+    array.forEach((element, index)=> {
+        data[`element${index}`] = element
+    });
+    return data
+}
+
+module.exports = {
+    getAll: (req, res) => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        carModel.getAll((err, results, fields) => {
+            if (err) throw err
+            
+            res.write(JSON.stringify(createJson(results[0])))
+            res.end()
+        })
+    }
+}
+
+
